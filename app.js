db546c7cea0574b0bd96bfc0bc431f1c489d5db0
orenbass <oren.bassman@elbitsystems.com>
2025-09-02 00:25:33 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("./service-worker.js").then(() => {
-        console.log("Service Worker Registered");
+        console.log("Service Worker Registered");   
     }).catch((error) => {
         console.error("Service Worker Registration Failed:", error);
     });
