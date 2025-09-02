cb40f59fd317ce8d42d7bb2cddcb233ddaae5237
orenbass <oren.bassman@elbitsystems.com>
2025-09-02 17:06:03 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -9,6 +9,7 @@ const urlsToCache = [
 ];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(urlsToCache);
@@ -16,11 +17,14 @@ self.addEventListener("install", (event) => {
   );
 });
 
+self.addEventListener("activate", (e) => {
+  clients.claim();
+});
+
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
-    
   );
 });
