4a6843c34a1c4f2ced976ae709fadd7eb88d84cf
orenbass <oren.bassman@elbitsystems.com>
2025-09-24 10:07:01 +0300

    Update google-drive-uploader.js

--- js/services/google-drive-uploader.js
+++ js/services/google-drive-uploader.js
@@ -1,7 +1,7 @@
 (function() {
   if (window.GoogleDriveUploader) return;
 
-  const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzd65c7vzrm0-XqtxWl-l7Yuh4RT4htdiETp_gn0KhVthr886rZtGqx3OyYPqu-8UU/exec';
+  const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzDL6ACluh4gnBF2BU_4JK8y4akpSHsTelOXeb-j32uZXaw7NWNRp8664QlzoLf7pM/exec';
 
   function blobToBase64(blob) {
     return new Promise((resolve, reject) => {
@@ -75,3 +75,4 @@
   window.GoogleDriveUploader = { upload };
 })();
 // ודא שאין שום קוד נוסף אחרי שורה זו
+
